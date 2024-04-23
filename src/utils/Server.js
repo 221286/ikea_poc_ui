76fb0b7d323aabaa578ad89dbcc724e98ef9e9a1
221286 <140632301+221286@users.noreply.github.com>
2024-04-23 12:34:27 +0530

    Adjusting the URL

---
+++ src/utils/Server.js
@@ -0,0 +1,8 @@
+export const Server={
+    "remote":{
+        LoginURL:"http://127.0.0.1:7000/auth/login",
+        SignupURL:"http://127.0.0.1:7000/auth/register",
+        GrantBucketAccessURL:"",
+        CreateBucketURL:"http://127.0.0.1:7000/bucket/create"
+    }
+}
